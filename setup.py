254744627c1f9560499d59885aa129601bc0a872
youval <youval@gmail.com>
2021-08-03 15:53:42 -0700

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='process_interupt_timer',
     version='0.0.1',
-    packages=['process_interupt_timer'],
-    package_dir={'process_interupt_timer': 'src'},
+    py_modules=['process_interupt_timer'],
+    package_dir={'': 'src'},
     url='https://github.com/youdar/process_interupt_timer',
     license='MIT',
     author='youval',
