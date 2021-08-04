35cc086179b6aa3789e41205bc40fef31a8df80d
youdar <youval@gmail.com>
2021-08-03 17:13:07 -0700

    Update process_interupt_timer.py

--- src/process_interupt_timer.py
+++ src/process_interupt_timer.py
@@ -5,11 +5,17 @@ class TimeOutException(Exception):
     pass
 
 
+class TimeOutError(Error):
+    pass
+
+
 class ProcessTimeOut(object):
 
-    def __init__(self):
+    def __init__(self, use_error=False):
         signal.signal(signal.SIGALRM, self.time_out_handler)
         self.TimeOutException = TimeOutException
+        self.TimeOutError = TimeOutError
+        self.use_error = use_error
         self.secs = 0
         self.minutes = 0
         self.hours = 0
@@ -18,7 +24,10 @@ class ProcessTimeOut(object):
         out_str = f'{self.hours} ' * (self.hours > 0)
         out_str += f'{self.minutes} ' * (self.minutes > 0)
         out_str += f'{self.secs} ' * (self.secs > 0)
-        raise TimeOutException('FLow run for more than {}'.format(out_str))
+        if self.use_error:
+            raise self.TimeOutError('FLow run for more than {}'.format(out_str))
+        else:
+            raise self.TimeOutException('FLow run for more than {}'.format(out_str))
 
     def start_timer(self, secs=0, minutes=0, hours=0):
         t = secs + 60 * minutes + hours * 3600
