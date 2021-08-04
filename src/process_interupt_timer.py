0b8c2ad04091d5a28f2a7152a1233bd0d94ee6ee
youdar <youval@gmail.com>
2021-08-03 17:05:18 -0700

    Update process_interupt_timer.py

--- src/process_interupt_timer.py
+++ src/process_interupt_timer.py
@@ -10,14 +10,21 @@ class ProcessTimeOut(object):
     def __init__(self):
         signal.signal(signal.SIGALRM, self.time_out_handler)
         self.TimeOutException = TimeOutException
+        self.secs = 0
+        self.minutes = 0
+        self.hours = 0
 
-    @staticmethod
-    def time_out_handler(signum, frame):
-        raise TimeOutException('FLow run for more than 20 hours...')
+    def time_out_handler(self, signum, frame):
+        out_str = f'{self.hours} ' * (self.hours > 0)
+        out_str += f'{self.minutes} ' * (self.minutes > 0)
+        out_str += f'{self.secs} ' * (self.secs > 0)
+        raise TimeOutException('FLow run for more than {}'.format(out_str))
 
-    @staticmethod
-    def start_timer(secs=0, minutes=0, hours=0):
+    def start_timer(self, secs=0, minutes=0, hours=0):
         t = secs + 60 * minutes + hours * 3600
+        self.secs = secs
+        self.minutes = minutes
+        self.hours = hours
         signal.alarm(t)
 
     @staticmethod
